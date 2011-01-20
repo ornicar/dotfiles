a2af5123cf1096ff2a47db4e3be6400e19224411
ornicar <thibault.duplessis@gmail.com>
2011-01-20 08:34:50 -0800

    [awesome] remove launcher menu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -44,24 +44,6 @@ for s = 1, screen.count() do
 end
 -- }}}
 
--- {{{ Menu
--- Create a laucher widget and a main menu
---myawesomemenu = {
-   --{ "manual", terminal .. " -e man awesome" },
-   --{ "edit config", editor_cmd .. " " .. awful.util.getdir("config") .. "/rc.lua" },
-   --{ "restart", awesome.restart },
-   --{ "quit", awesome.quit }
---}
-
---mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    --{ "open terminal", terminal }
-                                  --}
-                        --})
-
---mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
-                                     --menu = mymainmenu })
--- }}}
-
 -- {{{ Wibox
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })

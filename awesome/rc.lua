aaefded100e043f23b77561ce983d1148019d0d1
ornicar <thibault.duplessis@gmail.com>
2011-02-04 09:38:17 -0800

    [awesome] simplify rules

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -178,17 +178,8 @@ awful.rules.rules = {
       properties = { border_width = 0,
                      border_color = "#000000",
                      focus = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+                     keys = clientkeys }
+    }
 }
 -- }}}
 

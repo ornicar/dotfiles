52432d41cd117ccb56da800d2d1a0a98e8b5fd55
ornicar <thibault.duplessis@gmail.com>
2011-04-29 20:10:09 -0700

    [zsh] Tweak theme

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,4 +1,4 @@
-PROMPT='%{$fg_bold[red]%}➜%{$fg_bold[green]%}%p %{$fg[cyan]%}%c$(git_prompt_info)%{$reset_color%} %'
+PROMPT='%{$fg_bold[red]%}⚡%{$fg_bold[green]%}%p %{$fg[cyan]%}%c$(git_prompt_info)%{$reset_color%} %'
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg_bold[blue]%}±(%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%})%{$reset_color%}"

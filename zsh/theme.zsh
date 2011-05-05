3e07e0b0fa2cf94837b2e1c0d1762a6e6f8b24a6
ornicar <thibault.duplessis@gmail.com>
2011-05-05 10:46:45 -0700

    [zsh] Improve Git prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,3 +3,4 @@ PROMPT='%{$fg_bold[red]%}⚡%{$fg_bold[green]%}%p %{$fg[cyan]%}%c$(git_prompt_in
 ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg_bold[blue]%}±(%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%})%{$reset_color%}"
 MODE_INDICATOR="%{$fg_bold[yellow]%}<VI>%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[blue]%} "

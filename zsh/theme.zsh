9346e55586e22d031de386fbb2fc68d780deb666
ornicar <thibault.duplessis@gmail.com>
2011-05-06 00:18:38 -0700

    [zsh] Improve prompt theme

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,6 +1,8 @@
-PROMPT='%{$fg_bold[red]%}⚡%{$fg_bold[green]%}%p %{$fg[cyan]%}%c$(git_prompt_info)%{$reset_color%} %'
+returncode="%(?.%{$fg[yellow]%} ⚡ .%{$fg[red]%} ⚡ )"
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg_bold[blue]%}±(%{$fg[green]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%})%{$reset_color%}"
-MODE_INDICATOR="%{$fg_bold[yellow]%}<VI>%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[blue]%} "
+PROMPT='${returncode}%{$fg_bold[cyan]%}%C$(git_prompt_info)%{$reset_color%} %'
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$reset_color%}±(%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
+MODE_INDICATOR="%{$fg[yellow]%}<VI>%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[cyan]%} "

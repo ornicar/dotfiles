e4847dc326f3966ffb57f2592fd1a17d4dad721f
ornicar <thibault.duplessis@gmail.com>
2011-05-09 11:43:22 -0700

    [ttytter] Attempt to show incoming tweets using ratpoison

--- ttytter/notify.pl
+++ ttytter/notify.pl
@@ -1,11 +1,23 @@
-sub notifier_libnotifyperl {
+sub notifier_ratpoison {
     my $class = shift;
     my $text = shift;
     my $ref = shift; # not used in this version
 
-    my $escaped = `echo \"TTYtter: $class - $text\" | iconv -c -f utf-8 -t ISO-8859-1`;
+	if (!defined($class) || !defined($ratpoison_path)) {
+		# we are being asked to initialize
+		$ratpoison_path = &wherecheck("trying to find ratpoison",
+			"ratpoison",
+            "ratpoison must be installed to use this notifier\n");
+		if (!defined($class)) {
+			$class = 'TTYtter';
+			$text = 'ratpoison support activated';
+		}
+	}
 
-    `ratpoison -c \"echo $escaped\"`;
+    #my $escaped = `echo \"TTYtter: $class - $text\" | iconv -c -f utf-8 -t ISO-8859-1`;
+    my $escaped = "$class - $text";
+
+    `$ratpoison_path -c \"echo $escaped\"`;
 
     return 1;
 }

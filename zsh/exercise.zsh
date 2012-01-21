7c79d5483e67d3c6c1c43d4d529c3c096c775e1b
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-21 12:37:32 +0100

    [zsh] Tweak projects and aliases

--- zsh/exercise.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-exdoprod() {
-    ssh exerciseprod "cd /www/exercise-deploys/current/ && $*"
-}
---
+++ zsh/jirafe.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+alias jdb01="ssh -A ubuntu@db01.ec2.jirafe.net"
+alias jdb02="ssh -A ubuntu@db02.ec2.jirafe.net"
+alias jdev="ssh -A ubuntu@developer.jirafe.com"

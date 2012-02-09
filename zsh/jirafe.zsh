56947bf376ddee0f641e2dd691af4e12dad52488
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-02-09 10:16:33 +0100

    [zsh] Add connection to jirafe staging server

--- zsh/jirafe.zsh
+++ zsh/jirafe.zsh
@@ -3,3 +3,5 @@
 alias jdb01="ssh -A ubuntu@db01.ec2.jirafe.net"
 alias jdb02="ssh -A ubuntu@db02.ec2.jirafe.net"
 alias jdev="ssh -A ubuntu@developer.jirafe.com"
+
+alias jsdb01="ssh -A ubuntu@staging-db01.ec2.jirafe.net"

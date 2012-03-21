e345300f2f86c902543904af73c8fe35ea41d9ec
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-21 19:45:07 +0100

    [play] Change executable path

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -6,8 +6,7 @@
 export PATH="$HOME/bin:$PATH"
 export PATH="$DOTFILES/scripts:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
-export PATH=$PATH:/home/thib/play-2.0
-#export PATH=$PATH:/home/thib/bin/play2
+export PATH=$PATH:/home/thib/play-2.0-stable
 
 export BROWSER="firefox-beta-bin"
 export EDITOR='vim'

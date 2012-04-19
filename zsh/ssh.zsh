d3be963a17aa58dd6f1d8a0786579d83f4649cbb
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-19 19:03:32 +0200

    [zsh] Add jirafe staging app1 alias

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -4,5 +4,6 @@ alias jdb01="ssh -A ubuntu@db01.ec2.jirafe.net"
 alias jdb02="ssh -A ubuntu@db02.ec2.jirafe.net"
 alias jdev="ssh -A ubuntu@developer.jirafe.com"
 alias jsdb01="ssh -A ubuntu@staging-db01.ec2.jirafe.net"
+alias jsapp1="ssh -A ubuntu@staging-app1.ec2.jirafe.net"
 
 alias marty="ssh -A marty"

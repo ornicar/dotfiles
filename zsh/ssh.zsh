cbd64147270d9067977c8012fe7639451d90f69a
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-01 00:23:57 +0200

    [zsh] reorganize ssh connections, add .ssh/config file

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
-alias jdb01="ssh -A ubuntu@db01.ec2.jirafe.net"
-alias jdb02="ssh -A ubuntu@db02.ec2.jirafe.net"
-alias jdev="ssh -A ubuntu@developer.jirafe.com"
-alias jsdb01="ssh -A ubuntu@staging-db01.ec2.jirafe.net"
-alias jsapp1="ssh -A ubuntu@staging-app1.ec2.jirafe.net"
-alias jtest="ssh -A ubuntu@testing.ec2.jirafe.net"
-
-alias marty="ssh -A marty"
+alias jdb01="ssh -A jdb01"
+alias jdb02="ssh -A jdb02"
+alias jdev="ssh -A jdev"
+alias jsdb01="ssh -A jsdb01"
+alias jsapp1="ssh -A jsapp1"
+alias jtest="ssh -A jtest"
+alias jfreebird="ssh -A jfreebird"
+alias marty="ssh -A marty -t tmux attach"
+alias phobos="ssh phobos -t tmux attach"

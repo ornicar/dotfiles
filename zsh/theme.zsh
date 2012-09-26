e7d1bb04db113e4a0608d819555db80858d399d5
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-09-26 16:04:44 +0200

    [zsh] upgrade and unfuck oh-my-zsh, update prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,8 +1,15 @@
-returncode="%(?.%{$fg[yellow]%} ⚡ .%{$fg[red]%} ⚡ )"
-
-PROMPT='${returncode}%{$fg_bold[cyan]%}%C$(git_prompt_info)%{$reset_color%} %'
-
 ZSH_THEME_GIT_PROMPT_PREFIX=" %{$reset_color%}±(%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
 MODE_INDICATOR="%{$fg[yellow]%}<VI>%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[cyan]%} "
+
+my_git_prompt_info() {
+  [ ! -f .git/HEAD ] && return
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="*detached*"
+  if [ -f .git/BISECT_START ]; then bisect="*bisect*"; else bisect=""; fi
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$bisect$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
+
+returncode="%(?.%{$fg[yellow]%} ⚡ .%{$fg[red]%} ⚡ )"
+
+PROMPT='${returncode}%{$fg_bold[cyan]%}%C$(my_git_prompt_info)%{$reset_color%} %'

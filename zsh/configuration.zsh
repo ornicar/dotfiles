27240ecbd01840f7825d01fb1880ba022f8527a6
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-09-26 12:49:36 +0200

    [zsh] try to disable a JVM obscure option

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -15,10 +15,9 @@ export GIT_EDITOR='vim'
 # Java/SBT configuration
 export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
 #export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx512M -Xss2M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError"
-
 #export SBT_OPTS_CHEAP="-Dfile.encoding=UTF8 -Xms64M -Xmx128M -Xss2M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=64M"
 
-export _JAVA_AWT_WM_NONREPARENTING=1
+#export _JAVA_AWT_WM_NONREPARENTING=1
 
 # oh-my-zsh configuration
 export DISABLE_AUTO_TITLE="true"

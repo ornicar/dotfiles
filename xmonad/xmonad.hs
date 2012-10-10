3d41322197481ef5e6db432a2b878079918a9225
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-10-10 09:15:18 +0200

    [xmonad] tweak config using new haskell fu

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,16 +1,13 @@
 import XMonad
 import XMonad.Layout.NoBorders
-import XMonad.Layout.Accordion
 import XMonad.Util.EZConfig
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageHelpers
-import Graphics.X11.ExtraTypes.XF86
 
 import qualified XMonad.StackSet as W
 
-main = xmonad $ withUrgencyHook dzenUrgencyHook { args = ["-bg", "darkgreen", "-xs", "1"] }
-              $ myConfig
+main = xmonad $ withUrgencyHook dzenArgs myConfig
+  where dzenArgs = dzenUrgencyHook { args = ["-bg", "darkgreen", "-xs", "1"] }
 
 ------------------------------------------------------------------------
 -- Workspaces

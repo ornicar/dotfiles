d52f2600119f168e6688f5b698be2cb96fde4cad
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-04-01 17:55:25 -0300

    [zsh] show username if not "thib"

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -2,6 +2,8 @@ ZSH_THEME_GIT_PROMPT_PREFIX=" %{$reset_color%}±(%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
 MODE_INDICATOR="%{$fg[yellow]%}<VI>%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[cyan]%} "
+USER_NAME=$(whoami)
+[ $USER_NAME = "thib" ] && USER_NAME=""
 
 my_git_prompt_info() {
   [ ! -f .git/HEAD ] && return
@@ -10,6 +12,6 @@ my_git_prompt_info() {
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$bisect$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
-returncode="%(?.%{$fg[yellow]%} λ .%{$fg[red]%} λ )"
+returncode="%(?.%{$fg[yellow]%}$USER_NAME λ .%{$fg[red]%} λ )"
 
 PROMPT='${returncode}%{$fg_bold[cyan]%}%C$(my_git_prompt_info)%{$reset_color%} %'

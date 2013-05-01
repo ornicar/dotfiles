b7a47ac67f8ee058ae481c11806cb45fff585da3
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-05-01 20:39:56 -0300

    [zsh] make theme git infos faster

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,17 +1,17 @@
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$reset_color%}±(%{$fg[green]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
 MODE_INDICATOR="%{$fg[yellow]%}<VI>%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%{$reset_color%}%{$fg[cyan]%} "
 USER_NAME=$(whoami)
 [ $USER_NAME = "thib" ] && USER_NAME=""
 
 my_git_prompt_info() {
-  [ ! -f .git/HEAD ] && return
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="*detached*"
-  if [ -f .git/BISECT_START ]; then bisect="*bisect*"; else bisect=""; fi
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$bisect$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  ref=$(cat .git/HEAD 2> /dev/null) || return
+  branch=${ref#ref: refs/heads/} 
+  SHA=$(cat .git/refs/heads/$branch 2> /dev/null)
+  if [ -z $SHA ]; then
+    SHA=$branch
+    branch="*detached*"
+  fi
+  if [ -f .git/BISECT_START ]; then bisect=" *bisect*"; else bisect=""; fi
+  echo " %{$reset_color%}±(%{$fg[green]%}$branch %{$fg[cyan]%}$SHA[1,5]%{$reset_color%}$bisect%{$reset_color%})"
 }
 
-returncode="%(?.%{$fg[yellow]%}$USER_NAME λ .%{$fg[red]%} λ )"
-
-PROMPT='${returncode}%{$fg_bold[cyan]%}%C$(my_git_prompt_info)%{$reset_color%} %'
+PROMPT='%(?.%{$fg[yellow]%}$USER_NAME λ .%{$fg[red]%} λ )%{$fg_bold[cyan]%}%C$(my_git_prompt_info)%{$reset_color%} %'

9791624a4480fb8f8d3c63250111cad2e59fc383
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-12-18 13:40:48 +0100

    [acpi] tweak volume controls

--- acpi/mac-handler.sh
+++ acpi/mac-handler.sh
@@ -10,8 +10,8 @@ export PULSE_RUNTIME_PATH="/run/user/1000/pulse/"
 case "$1" in
   video/brightnessdown) /home/thib/.scripts/mac-brightness down;;
   video/brightnessup) /home/thib/.scripts/mac-brightness up;;
-  button/volumedown) for i in $(sudo -u thib -E pacmd list-sinks | grep index | sed 's/[^0-9]//g'); do echo "$i"; sudo -u thib -E pactl set-sink-volume "$i" -- -10%; done;;
-  button/volumeup) for i in $(sudo -u thib -E pacmd list-sinks | grep index | sed 's/[^0-9]//g'); do sudo -u thib -E pactl set-sink-volume "$i" -- +10%; done;;
+  button/volumedown) for i in $(sudo -u thib -E pacmd list-sinks | grep index | sed 's/[^0-9]//g'); do echo "$i"; sudo -u thib -E pactl set-sink-volume "$i" -- -5%; done;;
+  button/volumeup) for i in $(sudo -u thib -E pacmd list-sinks | grep index | sed 's/[^0-9]//g'); do sudo -u thib -E pactl set-sink-volume "$i" -- +5%; done;;
   button/mute) for i in $(sudo -u thib -E pacmd list-sinks | grep index | sed 's/[^0-9]//g'); do sudo -u thib -E pactl set-sink-mute "$i" -- toggle; done;;
   cd/play) sudo -u thib -E mpc toggle;;
   cd/next) sudo -u thib -E mpc next;;

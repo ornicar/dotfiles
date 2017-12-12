56b3962a414b3e92567aadefc6cf59b857432f9a
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-12-12 11:19:44 -0500

    new laptop, new dotfiles - XPS15 9560

--- i3/status.py
+++ i3/status.py
@@ -11,24 +11,15 @@ status = Status(standalone=True, click_events=False)
 #                          ^-- calendar week
 # status.register("clock", format = [ ('%a %b %-d %b %X', 'UTC'), ("%a %-d %b %R", "%X") ])
     # format="%a %-d %b %R",)
-status.register("clock", format = ('UTC>%H:%M', 'UTC'))
+status.register("clock", format = ('UTC %H:%M', 'UTC'))
 status.register("clock", format = ("%a %-d %b %R", "America/Bogota"))
     # format="%a %-d %b %R",)
 
-# The battery monitor has many formatting options, see README for details
-
-# This would look like this, when discharging (or charging)
-# ↓14.22W 56.15% [77.81%] 2h:41m
-# And like this if full:
-# =14.22W 100.0% [91.21%]
-#
-# This would also display a desktop notification (via dbus) if the percentage
-# goes below 5 percent while discharging. The block will also color RED.
 status.register("battery",
     format="{status}/{consumption:.2f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
     color="#ff8888",
-    full_color="#aaaaff",
-    charging_color="#00bb00",
+    full_color="#639B24",
+    charging_color="#639B24",
     alert=True,
     alert_percentage=10,
     status={
@@ -37,43 +28,6 @@ status.register("battery",
         "FULL": "=",
     },)
 
-# Displays whether a DHCP client is running
-# status.register("runwatch",
-#     name="DHCP",
-#     path="/var/run/dhclient*.pid",)
-
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
-# status.register("network",
-#     interface="eth0",
-#     format_up="{v4cidr}",
-#     format_down="{interface}",
-#     color_down="#888888")
-
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
-# status.register("disk",
-#     path="/",
-#     format="{avail}G",
-#     round_size=None)
-
-# status.register("network",
-#     interface="wlp2s0",
-#     format_up="{interface} {quality}%",
-#     graph_width=16,
-#     recv_limit=100,
-#     dynamic_color=False,
-#     color_up="#00bb00",
-#     color_down="#00bb00",
-#     start_color="#00bb00",
-#     graph_style="braille-fill")
-
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
 status.register("load",
@@ -81,27 +35,43 @@ status.register("load",
 
 status.register("mem",
     format="MEM {percent_used_mem}%",
-    color="#00bb00",
-    warn_percentage=90,
-    alert_percentage=95)
+    color="#639B24",
+    warn_percentage=80,
+    alert_percentage=90)
 
-# Shows your CPU temperature, if you have a Intel CPU
 status.register("temp",
-    file="/sys/class/hwmon/hwmon1/temp1_input",
+    file="/sys/devices/platform/coretemp.0/hwmon/hwmon2/temp1_input",
     interval=1,
     alert_temp=75,
     format="{temp:.0f}°",)
 
+# status.register("temp",
+#     file="/sys/class/hwmon/hwmon3/fan1_input",
+#     interval=1,
+#     alert_temp=3000,
+#     format="{temp:.0f}°",)
+
+# status.register("file",
+#     components={
+#         'rpm1': (int, "/sys/class/hwmon/hwmon3/fan1_input"),
+#         'rpm2': (int, "/sys/class/hwmon/hwmon3/fan2_input")
+#     },
+#     format="Fans {rpm1} {rpm2}")
+
+status.register("cpu_freq",
+    file="/sys",
+    interval=1,
+    format="{avgg} Ghz")
+
 status.register("cpu_usage_graph",
     format="CPU {usage:2}",
-    start_color="#00bb00"
+    start_color="#639B24"
 )
 
 # Shows pulseaudio default sink volume
-#
 # Note: requires libpulseaudio from PyPI
 status.register("pulseaudio",
-    format="Vol {volume}",)
+    format="{volume} ♪",)
 
 # Shows mpd status
 status.register("mpd",
@@ -111,6 +81,16 @@ status.register("mpd",
         "pause": "▷",
         "play": "▶",
         "stop": "◾",
-    },)
+    })
+
+status.register("network",
+    interface="wlp2s0",
+    format_up="{bytes_sent} kB/s↑ {bytes_recv} kB/s↓",
+    format_down="Internet DOWN",
+    dynamic_color = True,
+    start_color="#639B24",
+    end_color="yellow",
+    color_down="#ff2222",
+)
 
 status.run()

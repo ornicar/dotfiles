fa893d15f4540d67ce8cc41bc8754ef14ee4e9bc
Thibault Duplessis <thibault.duplessis@gmail.com>
2020-01-09 22:03:04 -0600

    ripgrep and kp

--- zsh/git-functions.zsh
+++ zsh/git-functions.zsh
@@ -43,3 +43,17 @@ git-set-upstream() {
 git() {
     hub "$@"
 }
+
+### PROCESS
+# mnemonic: [K]ill [P]rocess
+# show output of "ps -ef", use [tab] to select one or multiple entries
+# press [enter] to kill selected processes and go back to the process list.
+# or press [escape] to go back to the process list. Press [escape] twice to exit completely.
+kp() {
+  local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+    kp
+  fi
+}

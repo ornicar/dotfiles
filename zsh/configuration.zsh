94c0c397c8405a3aaa9fd977648e9bb92823a646
Thibault Duplessis <t@lichess.org>
2022-07-06 08:32:52 +0200

    use jvm-17 for dev

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -18,7 +18,7 @@ export GIT_EDITOR="nvim -u ~/dotfiles/nvim/gitcommit.init.vim"
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=default'"
 
-export JAVA_HOME=/usr/lib/jvm/java-11-graalvm
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export SBT_OPTS="-Xms256M -Xmx4G"
 
 export QT_QPA_PLATFORMTHEME=qt5ct

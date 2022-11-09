4dc6163823137ad51a3e63da28394b3a107e27a0
Thibault Duplessis <t@lichess.org>
2022-11-09 22:35:59 +0100

    nvim debugger

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,13 @@
-require'packer'.startup(function(use)
+require 'packer'.startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'kyazdani42/nvim-web-devicons'
   use 'neovim/nvim-lspconfig'
   use 'rakr/vim-one'
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
+  use { 'nvim-lualine/lualine.nvim' }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'nvim-telescope/telescope.nvim' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'kdheepak/lazygit.nvim'
   use 'machakann/vim-sandwich'
   -- use 'tpope/vim-fugitive' -- Git blame
@@ -20,13 +22,21 @@ require'packer'.startup(function(use)
   use 'stefandtw/quickfix-reflector.vim'
   use 'rust-lang/rust.vim'
   use 'onsails/lspkind-nvim'
-  use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
+  use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
   use({
     "hrsh7th/nvim-cmp",
-    requires = { { "hrsh7th/cmp-nvim-lsp" }, { "hrsh7th/cmp-buffer" }, { "hrsh7th/cmp-path" }, { "saadparwaiz1/cmp_luasnip" }, { "petertriho/cmp-git" }, { "ray-x/cmp-treesitter" } },
+    requires = { { "hrsh7th/cmp-nvim-lsp" }, { "hrsh7th/cmp-buffer" }, { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" }, { "petertriho/cmp-git" }, { "ray-x/cmp-treesitter" } },
   })
-  use {'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" } }
+  use 'mfussenegger/nvim-dap'
+  use { 'scalameta/nvim-metals' }
   use 'lukas-reineke/lsp-format.nvim'
+  use {
+    'pwntester/octo.nvim',
+    config = function()
+      require "octo".setup()
+    end
+  }
 end)
 
 require('theme')
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -134,6 +134,11 @@ _G.packer_plugins = {
     path = "/home/thib/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-dap"] = {
+    loaded = true,
+    path = "/home/thib/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/home/thib/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
@@ -153,6 +158,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/thib/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["octo.nvim"] = {
+    config = { "\27LJ\2\n2\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\tocto\frequire\0" },
+    loaded = true,
+    path = "/home/thib/.local/share/nvim/site/pack/packer/start/octo.nvim",
+    url = "https://github.com/pwntester/octo.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
@@ -232,6 +243,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: octo.nvim
+time([[Config for octo.nvim]], true)
+try_loadstring("\27LJ\2\n2\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\tocto\frequire\0", "config", "octo.nvim")
+time([[Config for octo.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then

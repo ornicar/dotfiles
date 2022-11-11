54ea6a76260edc8eb63e1dfc652f63e9cb541727
Thibault Duplessis <t@lichess.org>
2022-11-11 08:34:56 +0100

    nvim tweaks

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,24 +1,52 @@
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-local cmp = require'cmp'
-local luasnip = require'luasnip'
-local lspkind = require'lspkind'
+local cmp = require 'cmp'
+local cmp_buffer = require 'cmp_buffer'
+local luasnip = require 'luasnip'
+local lspkind = require 'lspkind'
+
+local get_bufnrs_unless_huge = function()
+  local buf = vim.api.nvim_get_current_buf()
+  if vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf)) > 1024 * 1024 then -- 1 Megabyte max
+    return {}
+  end
+  return { buf }
+end
 
 cmp.setup {
-  -- view = {            
+  -- view = {
   --   entries = "custom" -- can be "custom", "wildmenu" or "native"
   -- },
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
-    { name = "treesitter" },
-    { name = "path" },
-    { name = 'buffer', option = { keyword_length = 3 } }
+    { name = 'buffer', option = {
+      keyword_length = 2,
+      option = {
+        get_bufnrs = get_bufnrs_unless_huge
+      }
+    } },
+    -- { name = "treesitter" },
+    -- { name = "path" },
+  },
+  sorting = {
+    comparators = {
+      function(...) return cmp_buffer:compare_locality(...) end,
+      -- The rest of your comparators...
+    }
   },
   mapping = cmp.mapping.preset.insert({
+    -- ["<C-e>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     vim.api.nvim_command('write')
+    --     cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+    --   else
+    --     fallback()
+    --   end
+    -- end, { 'i', 's' }),
     ["<C-e>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
     ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false, },
-    ['<C-Space>'] = cmp.mapping.complete{ behavior = cmp.ConfirmBehavior.Replace, select = true, },
+    ['<C-Space>'] = cmp.mapping.complete { behavior = cmp.ConfirmBehavior.Replace, select = true, },
     -- ["<C-n>"] = cmp.mapping.select_next_item(),
     -- ["<C-k>"] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping(function()

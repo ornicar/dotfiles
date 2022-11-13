5703a169fbd04550ad64bfa310dee49294540b84
Thibault Duplessis <t@lichess.org>
2022-11-13 14:55:32 +0100

    nvim lua format

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 local builtin = require('telescope.builtin')
 
 map('n', '<leader>ms', builtin.find_files, {})
-map('n', '<leader>mr', function() builtin.find_files( { cwd = vim.fn.expand('%:p:h') }) end, {})
+map('n', '<leader>mr', function() builtin.find_files({ cwd = vim.fn.expand('%:p:h') }) end, {})
 map('n', '<leader>a', builtin.live_grep, {})
 map('n', '<leader>mb', builtin.buffers, {})
 map('n', '<leader>H', builtin.help_tags, {})
@@ -25,7 +25,7 @@ map("n", "<space>s", builtin.lsp_dynamic_workspace_symbols)
 map("n", "<space>d", builtin.diagnostics)
 
 -- :help telescope.setup()
-require'telescope'.setup{
+require 'telescope'.setup {
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
@@ -50,12 +50,12 @@ require'telescope'.setup{
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
     }
   }
 }
-require'telescope'.load_extension'fzf'
+require 'telescope'.load_extension 'fzf'

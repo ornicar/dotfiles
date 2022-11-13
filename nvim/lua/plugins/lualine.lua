ee8cd04d046426c09ee73df5e59bcef17362a8b1
Thibault Duplessis <t@lichess.org>
2022-11-13 08:56:15 +0100

    lualine tweak

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,13 @@
 require('lualine').setup {
   sections = {
-    lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
-    lualine_b = {'branch', 'diff',
-       { 'diagnostics', sources = { 'nvim_diagnostic', 'nvim_lsp' } }
+    lualine_a = { { 'mode', fmt = function(str) return str:sub(1, 1) end } },
+    lualine_b = { 'branch', 'diff',
+      { 'diagnostics', sources = { 'nvim_diagnostic', 'nvim_lsp' } }
     },
     lualine_c = {
-       { 'filename', path = 1 } ,
+      { 'filename', path = 3 },
       -- 'buffers'
     },
-    lualine_x = {'g:metals_status'}
+    lualine_x = { 'g:metals_status' }
   }
 }

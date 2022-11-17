a06c16d6e05d742635dfa2ef515062a7f1949489
Thibault Duplessis <t@lichess.org>
2022-11-17 19:10:50 +0100

    nvim dap

--- nvim/lua/plugins/metals.lua
+++ nvim/lua/plugins/metals.lua
@@ -3,13 +3,13 @@ local map = vim.keymap.set
 map('n', '<leader>mc', function() require "telescope".extensions.metals.commands() end)
 
 -- dap
--- map("n", "<leader>dc", [[<cmd>lua require"dap".continue()<CR>]])
--- map("n", "<leader>dr", [[<cmd>lua require"dap".repl.toggle()<CR>]])
--- map("n", "<leader>dK", [[<cmd>lua require"dap.ui.widgets".hover()<CR>]])
--- map("n", "<leader>dt", [[<cmd>lua require"dap".toggle_breakpoint()<CR>]])
--- map("n", "<leader>do", [[<cmd>lua require"dap".step_over()<CR>]])
--- map("n", "<leader>di", [[<cmd>lua require"dap".step_into()<CR>]])
--- map("n", "<leader>dl", [[<cmd>lua require"dap".run_last()<CR>]])
+map("n", "<leader>dc", [[<cmd>lua require"dap".continue()<CR>]])
+map("n", "<leader>dr", [[<cmd>lua require"dap".repl.toggle()<CR>]])
+map("n", "<leader>dK", [[<cmd>lua require"dap.ui.widgets".hover()<CR>]])
+map("n", "<leader>dt", [[<cmd>lua require"dap".toggle_breakpoint()<CR>]])
+map("n", "<leader>do", [[<cmd>lua require"dap".step_over()<CR>]])
+map("n", "<leader>di", [[<cmd>lua require"dap".step_into()<CR>]])
+map("n", "<leader>dl", [[<cmd>lua require"dap".run_last()<CR>]])
 
 local metals = require 'metals'
 local metals_config = metals.bare_config()
@@ -24,28 +24,28 @@ metals_config.init_options.statusBarProvider = "on"
 -- cmp integration
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- require 'dap'.configurations.scala = {
---   {
---     type = "scala",
---     request = "launch",
---     name = "RunOrTest",
---     metals = {
---       runType = "runOrTestFile",
---       --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
---     },
---   },
---   {
---     type = "scala",
---     request = "launch",
---     name = "Test Target",
---     metals = {
---       runType = "testTarget",
---     },
---   },
--- }
+require 'dap'.configurations.scala = {
+  {
+    type = "scala",
+    request = "launch",
+    name = "RunOrTest",
+    metals = {
+      runType = "runOrTestFile",
+      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+    },
+  },
+  {
+    type = "scala",
+    request = "launch",
+    name = "Test Target",
+    metals = {
+      runType = "testTarget",
+    },
+  },
+}
 
 metals_config.on_attach = function(client, bufnr)
-  -- require 'metals'.setup_dap()
+  require 'metals'.setup_dap()
   require 'lsp-format'.on_attach(client, bufnr)
 end
 

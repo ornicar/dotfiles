ae55f98fbf9015139250f9f89b0e60e2e0a9a02f
Thibault Duplessis <t@lichess.org>
2022-11-24 08:47:16 +0100

    nvim gitcommit config

--- nvim/init.gitcommit.lua
+++ nvim/init.gitcommit.lua
@@ -1,11 +1,16 @@
+require 'startup'
 local util = require 'packer.util'
+local map = vim.keymap.set
+
 require 'packer'.startup({ function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-lua/plenary.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'folke/tokyonight.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'kylechui/nvim-surround', config = function() require 'nvim-surround' end }
+  use { 'kylechui/nvim-surround', config = function() require 'nvim-surround'.setup() end }
+  use 'tpope/vim-commentary'
+
   use 'onsails/lspkind-nvim' -- lsp icons
   use({
     "hrsh7th/nvim-cmp",
@@ -15,6 +20,7 @@ require 'packer'.startup({ function(use)
   use 'kevinhwang91/nvim-hlslens'
   use 'RRethy/vim-illuminate'
   use 'zakharykaplan/nvim-retrail' -- trim trail whitespace
+  use { "gbprod/yanky.nvim", config = function() require 'yanky'.setup() end }
 end,
   config = {
     autoremove   = true,
@@ -32,12 +38,14 @@ require('plugins.cmp')
 require('plugins.treesitter')
 require('plugins.scrollbar')
 
+map('n', '<leader>c<space>', '<Plug>CommentaryLine')
+map('v', '<leader>c<space>', '<Plug>Commentary')
+
 vim.opt.number = false
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.list = false
 vim.opt.spell = true
-vim.opt.complete = true
 vim.opt_global.complete:append("kspell")
 
 vim.cmd([[

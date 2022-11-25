1e75b9b88a181aa65a20fa02163eee3c7b6cda78
Thibault Duplessis <t@lichess.org>
2022-11-25 12:20:05 +0100

    Revert "luasnip with custom scala snippets"
    This reverts commit 871f56f3e6f0b95af92eea278883a45e185b2f7e.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -3,13 +3,6 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 local cmp = require 'cmp'
 local cmp_buffer = require 'cmp_buffer'
 local lspkind = require 'lspkind'
-local luasnip = require 'luasnip'
-require 'luasnip.loaders.from_vscode'.lazy_load({
-  include = { 'scala', 'typescript' },
-})
-require 'luasnip.loaders.from_vscode'.lazy_load({
-  paths = { './snippets' }
-})
 
 local get_bufnrs_unless_huge = function()
   local buf = vim.api.nvim_get_current_buf()
@@ -25,7 +18,6 @@ cmp.setup {
   -- },
   sources = {
     { name = "nvim_lsp" },
-    { name = "luasnip" },
     { name = 'buffer', option = {
       keyword_length = 2,
       option = {
@@ -55,22 +47,18 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete { behavior = cmp.ConfirmBehavior.Replace, select = true, },
     -- ["<C-n>"] = cmp.mapping.select_next_item(),
     -- ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping(function(fallback)
+    ['<C-n>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
-        fallback()
+        -- fallback()
       end
     end, { 'i', 's' }),
-    ['<C-k>'] = cmp.mapping(function(fallback)
+    ['<C-k>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
-        fallback()
+        -- fallback()
       end
     end, { 'i', 's' }),
   }),
@@ -81,7 +69,6 @@ cmp.setup {
         buffer = "[Buf]",
         treesitter = "[Tree]",
         nvim_lsp = "[LSP]",
-        luasnip = "[Snip]",
       })
     }),
   },
@@ -99,8 +86,8 @@ cmp.setup {
   --   end
   -- },
   snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
+    expand = function()
+      -- error('snippet engine is not configured.')
     end,
   },
 }

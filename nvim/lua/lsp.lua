5cf397c6fc8194d21def8909e8cd906e45093411
Thibault Duplessis <t@lichess.org>
2023-01-19 15:13:05 +0100

    nvim: format scss with prettier

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -58,7 +58,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd.with({
-      filetypes = { "typescript", "javascript" },
+      filetypes = { "typescript", "javascript", "css", "scss" },
     }),
     -- null_ls.builtins.diagnostics.eslint,
   },

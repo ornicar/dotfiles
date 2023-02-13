f071eae9bb3ee6a00e26fb197fe47dbb713f3bb9
Thibault Duplessis <t@lichess.org>
2023-02-13 14:58:24 +0100

    lazyvim wip

---
+++ nvim/lua/plugins/core.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+      -- load the default settings
+      defaults = {
+        autocmds = true, -- lazyvim.config.autocmds
+        keymaps = false, -- lazyvim.config.keymaps
+        options = true, -- lazyvim.config.options
+      },
+    },
+  }
+}

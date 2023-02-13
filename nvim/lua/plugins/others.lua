0eed2ae141c1e4ffd27c62ed4e6799fbc361987e
Thibault Duplessis <t@lichess.org>
2023-02-13 09:39:30 +0100

    nvim lazy WIP

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -29,13 +29,11 @@ map('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<cr>')
 require("nvim-surround").setup({})
 
 
-vim.notify = require("notify")
-
 
 require 'yanky'.setup({
-  highlight = {
-    timer = 200
-  }
+    highlight = {
+        timer = 200
+    }
 })
 require 'telescope'.load_extension 'yank_history'
 map("n", "<leader>mp", ":Telescope yank_history<cr>")
@@ -53,18 +51,18 @@ map('n', '<leader>wg', ":Rg <C-R><C-W> ")
 
 
 require('illuminate').configure({
-  -- providers: provider used to get references in the buffer, ordered by priority
-  providers = {
-    'treesitter',
-    'regex',
-  },
-  -- delay: delay in milliseconds
-  delay = 100,
-  -- under_cursor: whether or not to illuminate under the cursor
-  under_cursor = true,
-  -- large_file_cutoff: number of lines at which to use large_file_config
-  -- The `under_cursor` option is disabled when this cutoff is hit
-  large_file_cutoff = 9999,
+    -- providers: provider used to get references in the buffer, ordered by priority
+    providers = {
+        'treesitter',
+        'regex',
+    },
+    -- delay: delay in milliseconds
+    delay = 100,
+    -- under_cursor: whether or not to illuminate under the cursor
+    under_cursor = true,
+    -- large_file_cutoff: number of lines at which to use large_file_config
+    -- The `under_cursor` option is disabled when this cutoff is hit
+    large_file_cutoff = 9999,
 })
 
 

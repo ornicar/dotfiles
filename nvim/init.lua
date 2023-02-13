5e5bb04bd1b187f48ddab795708c9f98c1efbb71
Thibault Duplessis <t@lichess.org>
2023-02-13 12:53:59 +0100

    lazyvim wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,2 @@
-require 'startup'
-
-require("lazy").setup("plugins")
-
-require('theme')
-require('settings')
-require('colemak')
-require('mappings')
-require('commands')
-require('functions')
-require('project')
-require('lsp')
-
--- require('plugins.lualine')
--- require('plugins.cmp')
--- require('plugins.metals')
--- require('plugins.treesitter')
--- require('plugins.telescope')
--- require('plugins.scrollbar')
--- require('plugins.unimpaired')
--- require('plugins.others')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")

cf73729b786b614472b511a5e8a44bdf0d6a98bd
Thibault Duplessis <t@lichess.org>
2023-02-19 13:11:03 +0100

    nvim perfect completion mappings

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -19,8 +19,17 @@ return {
       opts.mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-Space>"] = cmp.mapping.complete({}),
-        ["<C-e>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-Space>"] = function()
+          if cmp.visible() then
+            cmp.confirm({ select = true })
+          else
+            cmp.complete()
+          end
+        end,
+        ["<C-e>"] = function(fallback)
+          fallback()
+        end,
+        ["<C-c>"] = cmp.mapping.abort(),
       })
     end,
   },

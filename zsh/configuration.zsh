b20876c77c5731c6e8a2cf796db03eae23ad4629
Thibault Duplessis <t@lichess.org>
2023-02-24 15:39:44 +0100

    more kitty config

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -10,7 +10,7 @@ path+="$HOME/.bin:$PATH"
 path+="$HOME/.local/share/coursier/bin"
 # path+="$HOME/.local/bin:$PATH" # crashes ZSH!!!
 
-export TERMINAL="kitty"
+export TERMINAL="kitty -1"
 export BROWSER="google-chrome-stable"
 export CHROME_EXECUTABLE="google-chrome-stable"
 export EDITOR='nvim'

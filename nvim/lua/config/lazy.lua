74d3ec5a14d0b9cd89205534e0ad34a8757e98f6
Thibault Duplessis <t@lichess.org>
2023-05-25 08:15:35 +0200

    nvim enable lazyvim dap extension

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -9,6 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },

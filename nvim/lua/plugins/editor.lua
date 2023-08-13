6b3f43e16192d2fc663168e8eb27239d613898b3
Thibault Duplessis <t@lichess.org>
2023-08-13 11:23:13 +0200

    nvim flash tweaks

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -23,10 +23,11 @@ return {
       labels = "nteshdlpufiryw",
       modes = {
         char = {
+          enabled = false,
           label = { exclude = "rdc" },
         },
         treesitter = {
-          label = { exclude = "rdc", before = false, after = false },
+          label = { before = false, after = false },
         },
       },
     },
@@ -37,7 +38,7 @@ return {
         { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         -- { "r", mode = "o", function() requiee("flash").remote() end, desc = "Remote Flash" },
         -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     }
     end,
   },

2c35530f9b597319ca47b4de3987df52556446eb
Thibault Duplessis <t@lichess.org>
2023-09-24 10:55:48 +0200

    java 21

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -18,7 +18,7 @@ export GIT_EDITOR="nvim"
 
 export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=default'"
 
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-bin
 export SBT_OPTS="-Xms256M -Xmx2G"
 
 export QT_QPA_PLATFORMTHEME=qt5ct

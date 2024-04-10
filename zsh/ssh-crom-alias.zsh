22e04eb235135535942ee9649da7d3f2eda1956c
Thibault Duplessis <t@lichess.org>
2024-04-10 16:27:31 +0200

    ssh crom aliases

--- zsh/ssh-crom-alias.zsh
+++ zsh/ssh-crom-alias.zsh
@@ -1,9 +1,11 @@
 
 sshcd () { ssh -t "$1" "cd \"$2\"; exec \$SHELL -l"; }
 sshrun () { ssh "$1" "cd \"$2\"; \$3"; }
+moshcd () { mosh -t "$1" "cd \"$2\"; exec \$SHELL -l"; }
+moshrun () { mosh "$1" "cd \"$2\"; \$3"; }
 
-alias cr='ssh crom'
-alias crlila='sshcd crom ~/lila'
-alias crlog='ssh crom "cd ~/lila; pnpm multilog"'
-alias crvim='ssh crom "cd ~/lila; nvim"'
+alias cr='mosh crom'
+alias crlila='moshcd crom ~/lila'
+alias crlog='mosh crom "cd ~/lila; pnpm multilog"'
+# alias crvim='mosh crom "cd ~/lila; nvim"'
 alias crlistart='ssh crom "systemctl --user restart lila'

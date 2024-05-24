46ac2356b1707193e45ee4a4fa255cfd587dcf41
Thibault Duplessis <t@lichess.org>
2024-05-24 20:32:16 +0200

    boot.nix

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }: {
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 }

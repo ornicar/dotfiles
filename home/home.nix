7fb55eb23a080d7cf92e81a6136eb4cf64301e03
Thibault Duplessis <t@lichess.org>
2024-05-24 22:19:43 +0200

    git.nix

--- home/home.nix
+++ home/home.nix
@@ -9,10 +9,7 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
+    ./git.nix
     ./kitty.nix
   ];
 

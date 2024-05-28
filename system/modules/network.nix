645c2b1589978d56741d8b748c33a049267f7ccf
Thibault Duplessis <t@lichess.org>
2024-05-28 10:27:15 +0200

    nm-applet

--- system/modules/network.nix
+++ system/modules/network.nix
@@ -1,4 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
 
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  environment.systemPackages = with pkgs; [
+    networkmanagerapplet
+  ];
+
+  networking.networkmanager.enable = true;
+
+  programs.nm-applet.enable = true;
 }

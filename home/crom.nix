a212a8bf02a246670a48378db309bb51bfb090e0
Thibault Duplessis <t@lichess.org>
2024-05-29 21:18:38 +0200

    wine & wlr-randr

--- home/crom.nix
+++ home/crom.nix
@@ -17,6 +17,14 @@ bindsym F10 exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0';
 bindsym ${modifier}+F10 exec 'screenshot.sh clipboard';
 '';
 
+  home.packages = with pkgs; [
+    (wineWowPackages.full.override {
+      wineRelease = "staging";
+      mingwSupport = true;
+    })
+    winetricks
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
 }
--- system/modules/packages.nix
+++ system/modules/packages.nix
@@ -2,6 +2,7 @@
 
   environment.systemPackages = with pkgs; [
     wayland
+    wlr-randr
     wget
     tldr
     xdg-utils

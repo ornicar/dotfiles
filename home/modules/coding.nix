e6ccc4ce79d28099e0a6466dd6ec49bd7d667dc8
Thibault Duplessis <t@lichess.org>
2024-05-29 21:32:29 +0200

    fix SASS_PATH

--- home/modules/coding.nix
+++ home/modules/coding.nix
@@ -2,7 +2,7 @@
 {
   home.sessionVariables = {
     # for lila/ui/build to find the sass executable
-    SASS_PATH = pkgs.dart-sass;
+    SASS_PATH = "${pkgs.dart-sass}/bin/dart-sass";
   };
 
   home.file = {

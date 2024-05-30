b922ed7aaf2a3ec2729514e3aee9ad45eb29205e
Thibault Duplessis <t@lichess.org>
2024-05-30 09:24:29 +0200

    upgrade lazyvim, replaces tsserver with vtsls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -19,43 +19,6 @@ return {
       servers = {
         lua_ls = {},
         nil_ls = {},
-        tsserver = {
-          keys = {
-            {
-              "<leader>co",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.organizeImports.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "Organize Imports",
-            },
-            {
-              "<leader>cR",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.removeUnused.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "Remove Unused Imports",
-            },
-          },
-          settings = {
-            typescript = {},
-            javascript = {},
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
-        },
       },
     },
   },

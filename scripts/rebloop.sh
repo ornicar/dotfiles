31d96d1d86d14ac0b4469336c4d46b5b97e912ab
Thibault Duplessis <t@lichess.org>
2024-05-30 11:20:54 +0200

    fix scripts

--- scripts/rebloop.sh
+++ scripts/rebloop.sh
@@ -1,1 +1,12 @@
-/nix/store/h7mbga7pa9k5x2rak6whq0lk07ci2nss-home-manager-files/.local/bin/rebloop.sh
+#!/bin/sh
+
+systemctl --user stop bloop
+fuser -k 8212/tcp -KILL
+find /tmp -name 'bsp-launcher*' -or -name -'metals*' -or -name 'bloop*' | xargs rm -rf {}
+rm -rf .metals
+rm -rf .bloop
+rm -rf target
+rm -rf modules/*/target
+systemctl --user restart bloop
+sleep 1
+systemctl --user status bloop
--- scripts/screenshot-file.sh
+++
@@ -1,1 +0,0 @@
-/nix/store/h7mbga7pa9k5x2rak6whq0lk07ci2nss-home-manager-files/.local/bin/screenshot-file.sh

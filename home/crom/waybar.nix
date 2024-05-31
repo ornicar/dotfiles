eb17aa376891bbf37b69ca5feac8acb6d64a1006
Thibault Duplessis <t@lichess.org>
2024-05-31 10:30:57 +0200

    waybar

--- home/crom/waybar.nix
+++ home/crom/waybar.nix
@@ -1,32 +1,49 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.waybar = {
     settings = {
       mainBar = {
         modules-right = [
           "cpu"
+          "temperature#cpu"
+          "custom/amdgpu"
           "memory"
-          "amdgpu"
           "network"
           "pulseaudio"
           "tray"
           "clock#utc"
           "clock#local"
         ];
-      };
-      cpu.format =
-        "{icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15}{icon16}{icon17}{icon18}{icon19}{icon20}{icon21}{icon22}{icon23}";
-      "amdgpu" = {
-        interval = 1;
-        exec = let
-          device = "amdgpu-pci-0800";
-          sensors = "${pkgs.lm_sensors}/bin/sensors";
-          sed = "${pkgs.gnused}/bin/sed";
-        in pkgs.writeShellScript "thib-amdgpu-waybar" ''
-          sens=$(${sensors} ${device})
-          power=$(echo $sens | ${sed} -rn 's/.*PPT:\s+([0-9]+).*/\1/p')
-          temp=$(echo $sens | ${sed} -rn 's/.*edge:\s+.([0-9]+).*/\1/p')
-          echo "$\{power\}W $\{temp\}°C"
-        '';
+        "cpu" = {
+          interval = 1;
+          format =
+            "{icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15}{icon16}{icon17}{icon18}{icon19}{icon20}{icon21}{icon22}{icon23}";
+          format-icons = [ "" "▁" "▂" "▃" "▄" "▅" "▆" "▇" "█" ];
+          states = {
+            warning = 20;
+            critical = 50;
+          };
+        };
+        "temperature#cpu" = {
+          interval = 1;
+          hwmon-path = "/sys/class/hwmon/hwmon2/temp1_input";
+          critical-threshold = 60;
+          format = "{icon} {temperatureC}°";
+          format-icons = [ "" ];
+        };
+        "custom/amdgpu" = {
+          interval = 2;
+          format = "GPU {}";
+          exec = let
+            device = "amdgpu-pci-0800";
+            sensors = "${pkgs.lm_sensors}/bin/sensors";
+            sed = "${pkgs.gnused}/bin/sed";
+          in pkgs.writeShellScript "thib-amdgpu-waybar" ''
+            sens=$(${sensors} ${device})
+            power=$(echo $sens | ${sed} -rn 's/.*PPT:\s+([0-9]+).*/\1/p')
+            temp=$(echo $sens | ${sed} -rn 's/.*edge:\s+.([0-9]+).*/\1/p')
+            echo "''${power}W $temp°"
+          '';
+        };
       };
     };
   };
--- home/fw/waybar.nix
+++ home/fw/waybar.nix
@@ -15,44 +15,52 @@
           "clock#utc"
           "clock#local"
         ];
-      };
-      cpu.format =
-        "{icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15}";
-      "temperature#cpu" = {
-        interval = 1;
-        hwmon-path = "/sys/class/thermal/thermal_zone3/temp";
-        critical-threshold = 70;
-        format = "{icon} {temperatureC}°";
-        format-icons = [ "" ];
-      };
-      "backlight" = {
-        format = "{icon} {percent}%";
-        format-icons = [ "" "" "" "" "" "" "" "" "" ];
-      };
-      "battery" = {
-        interval = 5;
-        states = {
-          good = 100;
-          warning = 50;
-          critical = 25;
+        "cpu" = {
+          interval = 1;
+          format =
+            "{icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15}";
+          format-icons = [ "" "▁" "▂" "▃" "▄" "▅" "▆" "▇" "█" ];
+          states = {
+            warning = 20;
+            critical = 50;
+          };
         };
-        format = "{icon} {capacity}% {power:2.1f}W";
-        format-charging = " {capacity}% {power:2.1f}W";
-        format-plugged = " {capacity}% {power:2.1f}W";
-        format-alt = "{icon} {time}";
-        format-icons = [ "" "" "" "" "" ];
-      };
-      "power-profiles-daemon" = {
-        format = "{icon}";
-        tooltip-format = ''
-          Power profile: {profile}
-          Driver: {driver}'';
-        tooltip = true;
-        format-icons = {
-          default = "";
-          performance = "";
-          balanced = "";
-          "power-saver" = "";
+        "temperature#cpu" = {
+          interval = 1;
+          hwmon-path = "/sys/class/thermal/thermal_zone3/temp";
+          critical-threshold = 70;
+          format = "{icon} {temperatureC}°";
+          format-icons = [ "" ];
+        };
+        "backlight" = {
+          format = "{icon} {percent}%";
+          format-icons = [ "" "" "" "" "" "" "" "" "" ];
+        };
+        "battery" = {
+          interval = 5;
+          states = {
+            good = 100;
+            warning = 50;
+            critical = 25;
+          };
+          format = "{icon} {capacity}% {power:2.1f}W";
+          format-charging = " {capacity}% {power:2.1f}W";
+          format-plugged = " {capacity}% {power:2.1f}W";
+          format-alt = "{icon} {time}";
+          format-icons = [ "" "" "" "" "" ];
+        };
+        "power-profiles-daemon" = {
+          format = "{icon}";
+          tooltip-format = ''
+            Power profile: {profile}
+            Driver: {driver}'';
+          tooltip = true;
+          format-icons = {
+            default = "";
+            performance = "";
+            balanced = "";
+            "power-saver" = "";
+          };
         };
       };
     };

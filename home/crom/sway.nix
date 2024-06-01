5f7c52f1d9a19e89b04cb27d4f2611dbbbc345c3
Thibault Duplessis <t@lichess.org>
2024-06-01 08:10:38 +0200

    coolercontrol auth

--- home/crom/sway.nix
+++ home/crom/sway.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }: {
+{ config, pkgs, ... }: {
 
   wayland.windowManager.sway = {
     extraConfig =
@@ -14,16 +14,16 @@
   services.swayidle = {
     enable = true;
     timeouts = let
-      # http post localhost:11987/login -a CCAdmin:coolAdmin
-      cooler-cookie =
-        "cc=ZFgH++e8rWPQNmKMqzF5z6ElsbcUxANwzMnCEZtdyGdT2WFwlZiTNXmiObaYNjIxGh2R5tgrwQ%3D%3D";
-      cooler-uid-off = "d7e75992-c2ce-473f-bbc7-0dd23c00ae07";
-      cooler-uid-on = "d14d565f-c9da-403d-bf54-7476379eb798";
+      api-url = "http://localhost:11987";
+      uid-off = "d7e75992-c2ce-473f-bbc7-0dd23c00ae07";
+      uid-on = "d14d565f-c9da-403d-bf54-7476379eb798";
+      cookie-file = "/tmp/coolercontrol-cookie";
       crom-mode = pkgs.writeShellScript "crom-mode.sh" ''
         mode=$1
         ${pkgs.sway}/bin/swaymsg "output * power $mode" 
-        if [ "$mode" = "off" ]; then uid=${cooler-uid-off}; else uid=${cooler-uid-on}; fi
-        ${pkgs.curl}/bin/curl -XPOST -H "cookie: ${cooler-cookie}" "localhost:11987/modes-active/$uid"
+        if [ "$mode" = "off" ]; then uid=${uid-off}; else uid=${uid-on}; fi
+        ${pkgs.curl}/bin/curl -XPOST -c ${cookie-file} ${api-url}/login -u CCAdmin:coolAdmin
+        ${pkgs.curl}/bin/curl -XPOST -b ${cookie-file} ${api-url}/modes-active/$uid
       '';
     in [{
       timeout = 600;

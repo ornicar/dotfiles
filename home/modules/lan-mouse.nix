68d4f34389852a34470fc0ebcee142b149d8d19e
Thibault Duplessis <t@lichess.org>
2024-06-02 10:23:43 +0200

    lan-mouse config

--- home/modules/lan-mouse.nix
+++ home/modules/lan-mouse.nix
@@ -2,9 +2,21 @@
   imports = [ inputs.lan-mouse.homeManagerModules.default ];
   programs.lan-mouse = {
     enable = true;
-    # systemd = false;
+    systemd = true;
     # package = inputs.lan-mouse.packages.${pkgs.stdenv.hostPlatform.system}.default
     # Optional configuration in nix syntax, see config.toml for available options
-    # settings = { };
+    settings = {
+      release_bind = [ "KeyLeftCtrl" "Key5" ];
+      port = 4242;
+      frontend = "cli";
+      right = {
+        hostname = "crom";
+        activate_on_startup = true;
+      };
+      left = {
+        hostname = "fw";
+        activate_on_startup = true;
+      };
+    };
   };
 }

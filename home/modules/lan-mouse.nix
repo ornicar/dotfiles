6c417e574a0d5dfb72dfc81b78d7f722d1e40701
Thibault Duplessis <t@lichess.org>
2024-06-02 10:23:43 +0200

    lan-mouse config

--- home/modules/lan-mouse.nix
+++ home/modules/lan-mouse.nix
@@ -1,10 +1,32 @@
-{ inputs, ... }: {
+{ inputs, pkgs, lib, ... }: {
   imports = [ inputs.lan-mouse.homeManagerModules.default ];
   programs.lan-mouse = {
     enable = true;
-    # systemd = false;
+    systemd = true;
     # package = inputs.lan-mouse.packages.${pkgs.stdenv.hostPlatform.system}.default
     # Optional configuration in nix syntax, see config.toml for available options
-    # settings = { };
+    settings = let
+      shareClipboard = dest:
+        "wl-paste --no-newline | ssh ${dest} env WAYLAND_DISPLAY='wayland-1' wl-copy";
+    in {
+      release_bind = [ "KeyLeftCtrl" "Key5" ];
+      port = 4242;
+      frontend = "cli";
+      right = {
+        hostname = "crom";
+        activate_on_startup = true;
+        enter_hook = shareClipboard "crom";
+      };
+      left = {
+        hostname = "fw";
+        activate_on_startup = true;
+        enter_hook = shareClipboard "fw";
+      };
+    };
   };
+
+  # we can't use any ${pkgs} proper path,
+  # because it also runs commands on the remote machine
+  systemd.user.services.lan-mouse.Service.Environment =
+    "PATH=$PATH:/run/current-system/sw/bin";
 }

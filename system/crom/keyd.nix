501e8ed578c2b35c64c2da514ff06d41a969d2e6
Thibault Duplessis <t@lichess.org>
2024-06-04 09:13:27 +0200

    zulip prev/next with the mouse buttons

--- system/crom/keyd.nix
+++ system/crom/keyd.nix
@@ -2,12 +2,18 @@
   services.keyd = {
     enable = true;
     keyboards = {
-      default = {
-        # 16c0:27db MoErgo Glove80
+      glove = {
         ids = [ "16c0:27db" ];
+        settings = { shift = { tab = "S-insert"; }; };
+      };
+      kone = {
+        ids = [ "1e7d:afca" ];
         settings = {
-          main = { shift = "layer(shift)"; };
-          "shift:G" = { tab = "S-insert"; };
+          main = {
+            # enables prev/next in zulip
+            mouse1 = "A-left";
+            mouse2 = "A-right";
+          };
         };
       };
     };

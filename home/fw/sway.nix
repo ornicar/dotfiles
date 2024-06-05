69cdae62d022a6a4dc7794bdc381c859d76f907c
Thibault Duplessis <t@lichess.org>
2024-06-05 09:24:58 +0200

    spotify keyboard control

--- home/fw/sway.nix
+++ home/fw/sway.nix
@@ -12,22 +12,23 @@
           pointer_accel = "0.3"; # set mouse sensitivity (between -1 and 1)
         };
       };
-      keybindings =
-        let inherit (config.wayland.windowManager.sway.config) modifier menu;
-        in lib.mkAfter {
-          "${modifier}+equal" = "exec ${menu}";
-          "XF86MonBrightnessDown" = "exec 'light -U 10'";
-          "XF86MonBrightnessUp" = "exec 'light -A 10'";
-          "XF86AudioRaiseVolume" =
-            "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
-          "XF86AudioLowerVolume" =
-            "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0'";
-          "XF86AudioMute" = "exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'";
-          "XF86AudioPlay" = "exec 'playerctl play-pause'";
-          "XF86AudioNext" = "exec 'playerctl next'";
-          "XF86AudioPrev" = "exec 'playerctl previous'";
-          "Print" = "exec 'screenshot.sh clipboard'";
-        };
+      keybindings = let
+        inherit (config.wayland.windowManager.sway.config) modifier menu;
+        spotify = "${pkgs.playerctl}/bin/playerctl -p spotify";
+      in lib.mkAfter {
+        "${modifier}+equal" = "exec ${menu}";
+        "XF86MonBrightnessDown" = "exec 'light -U 10'";
+        "XF86MonBrightnessUp" = "exec 'light -A 10'";
+        "XF86AudioRaiseVolume" =
+          "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
+        "XF86AudioLowerVolume" =
+          "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0'";
+        "XF86AudioMute" = "exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'";
+        "XF86AudioPlay" = "exec '${spotify} play-pause'";
+        "XF86AudioNext" = "exec '${spotify} next'";
+        "XF86AudioPrev" = "exec '${spotify} previous'";
+        "Print" = "exec 'screenshot.sh clipboard'";
+      };
     };
   };
 

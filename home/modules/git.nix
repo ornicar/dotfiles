b060832c25838dc542957c54566fe897cd987367
Thibault Duplessis <t@lichess.org>
2024-06-05 13:40:01 +0200

    git aliases

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -27,6 +27,36 @@
       blame.date = "relative";
     };
 
+    aliases = {
+      a = "add";
+      c = "commit";
+      ca = "commit --amend";
+      can = "commit --amend --no-edit";
+      cl = "clone";
+      cm = "commit -m";
+      co = "checkout";
+      cp = "cherry-pick";
+      cpx = "cherry-pick -x";
+      d = "diff";
+      f = "fetch";
+      fo = "fetch origin";
+      fu = "fetch upstream";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      pl = "pull";
+      pr = "pull -r";
+      ps = "push";
+      psf = "push -f";
+      rb = "rebase";
+      rbi = "rebase -i";
+      r = "remote";
+      ra = "remote add";
+      rr = "remote rm";
+      rv = "remote -v";
+      rs = "remote show";
+      st = "status";
+    };
+
     diff-so-fancy.enable = true;
     ignores = [
       ".idea"

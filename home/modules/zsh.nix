17aa38c80dc59d7f80ef9f6f900be483742ad7dd
Thibault Duplessis <t@lichess.org>
2024-06-05 11:03:59 +0200

    mongo-ssh.sh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -158,10 +158,6 @@
       "gw" = "git wtf";
       # Stuff
       "st" = "_ systemctl-tui";
-      "mongo-ssh" =
-        "ssh -fNL 27117:rodan.vrack.lichess.ovh:27017 root@rodan.lichess.ovh";
-      "mongo-ssh-pri" =
-        "ssh -NL 27917:kaiju.vrack.lichess.ovh:27017 root@kaiju.lichess.ovh";
       "on" = ''swaymsg " output * power on"'';
     };
   };
---
+++ scripts/mongo-ssh.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+server=$1
+background="-fNL"
+foreground="-NL"
+
+if [ "$server" = "sec" ]; then
+
+	host="rodan"
+	ssh $background 27117:$host.vrack.lichess.ovh:27017 root@$host.lichess.ovh
+
+elif [ "$server" = "study" ]; then
+
+	host="study"
+	ssh $background 27118:$host.vrack.lichess.ovh:27017 root@$host.lichess.ovh
+
+elif [ "$server" = "pri" ]; then
+
+	host="kaiju"
+	ssh $foreground 27917:$host.vrack.lichess.ovh:27017 root@$host.lichess.ovh
+
+fi

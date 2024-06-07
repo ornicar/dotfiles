9d3f511217a29d71109602b2be5e1888ea035b7c
Thibault Duplessis <t@lichess.org>
2024-06-07 07:29:38 +0200

    bloop options

--- home/services/bloop.nix
+++ home/services/bloop.nix
@@ -2,14 +2,17 @@
   systemd.user.services.bloop = {
     Unit = { Description = "Bloop Scala build server"; };
     Service = {
-      Environment = [
-        "PATH=${lib.makeBinPath [ config.programs.java.package ]}"
-        ''
-          JAVA_OPTS="-Xms4g -Xmx14G -XX:+UseG1GC -Xss2m -XX:InitialCodeCacheSize=512m -XX:ReservedCodeCacheSize=512m"''
-      ];
+      Environment =
+        [ "PATH=${lib.makeBinPath [ config.programs.java.package ]}" ];
       ExecStart = "${pkgs.bloop}/bin/bloop server";
       ExecStop = "${pkgs.psmisc}/bin/fuser -k 8212/tcp -TERM";
     };
     Install = { WantedBy = [ "default.target" ]; };
   };
+  # https://scalacenter.github.io/bloop/docs/server-reference#custom-java-options
+  home.file.".bloop/bloop.json".text = ''
+    {
+      "javaOptions": ["-Xms4g", "-Xmx14G", "-XX:+UseG1GC", "-Xss2m", "-XX:InitialCodeCacheSize=512m", "-XX:ReservedCodeCacheSize=512m"]
+    }
+  '';
 }

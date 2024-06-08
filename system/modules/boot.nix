2d9a687bcdc328e3c18ef57a1ff91bed06e37c42
Thibault Duplessis <t@lichess.org>
2024-06-08 14:33:54 +0200

    kernel parameters

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -7,6 +7,13 @@
       timeout = 1;
     };
     kernelPackages = pkgs.linuxPackages_latest;
-    kernel.sysctl = { "fs.inotify.max_user_watches" = "1048576"; };
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "1048576";
+      "vm.dirty_writeback_centisecs" = "2500";
+      # https://www.techtarget.com/searchdatacenter/definition/Linux-swappiness
+      # default was 60
+      "vm.swappiness" = "30";
+    };
+    kernelParams = [ "nmi_watchdog=0" ];
   };
 }

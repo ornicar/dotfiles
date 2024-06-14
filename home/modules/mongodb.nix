a88ce59a01dd9679f149e9082d1a914a51f4a780
Thibault Duplessis <t@lichess.org>
2024-06-14 22:50:25 +0200

    mongoshrc

--- home/modules/mongodb.nix
+++ home/modules/mongodb.nix
@@ -1,6 +1,8 @@
 { ... }: {
-  home.file.".mongoshrc".text = # js
+  home.file.".mongoshrc.js".text = # js
     ''
       const sec = () => connect('mongodb://localhost:27117/lichess');
+
+      const secImport = (coll, id) => db.getCollection(coll).insertOne(sec().getCollection(coll).findOne({ _id: id }));
     '';
 }

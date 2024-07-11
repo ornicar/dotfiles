a2827491ef8327e1b7e9fcfe74f5b5fa4e9ef744
Thibault Duplessis <t@lichess.org>
2024-07-11 10:04:25 +0200

    crom coolercontrol

--- home/crom/sway.nix
+++ home/crom/sway.nix
@@ -20,16 +20,20 @@
     timeouts = let
       api-url = "http://localhost:11987";
       uid-off = "d7e75992-c2ce-473f-bbc7-0dd23c00ae07";
-      uid-on = "d14d565f-c9da-403d-bf54-7476379eb798";
+      # uid-on = "d14d565f-c9da-403d-bf54-7476379eb798";
+      uid-on = "f9194efe-a828-4672-845b-21800f0013f0"; # summer
       cookie-file = "/tmp/coolercontrol-cookie";
       crom-mode = pkgs.writeShellScript "crom-mode.sh" ''
         mode=$1
-        if [ "$mode" = "off" ]; then uid=${uid-off}; else uid=${uid-on}; fi
-        ${pkgs.curl}/bin/curl --silent -XPOST -c ${cookie-file} ${api-url}/login -u CCAdmin:coolAdmin
-        ${pkgs.curl}/bin/curl --silent -XPOST -b ${cookie-file} ${api-url}/modes-active/$uid
         # hack to get the current sway socket as I've seen it be wrong once.
         export SWAYSOCK=$XDG_RUNTIME_DIR/sway-ipc.$UID.$(${pkgs.procps}/bin/pgrep -x sway).sock
         ${pkgs.sway}/bin/swaymsg "output * power $mode" 
+
+        # and now coolercontrol
+        # ${pkgs.coreutils}/bin/sleep 0
+        if [ "$mode" = "off" ]; then uid=${uid-off}; else uid=${uid-on}; fi
+        ${pkgs.curl}/bin/curl --silent -XPOST -c ${cookie-file} ${api-url}/login -u CCAdmin:coolAdmin
+        ${pkgs.curl}/bin/curl --silent -XPOST -b ${cookie-file} ${api-url}/modes-active/$uid
       '';
     in [{
       timeout = 600;

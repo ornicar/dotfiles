4d3e58b9ec8525d8885443e5785d1e742d9ba136
Thibault Duplessis <t@lichess.org>
2024-07-12 11:59:31 +0200

    bloop overlay doesn't work

--- system/overlays.nix
+++ system/overlays.nix
@@ -1,11 +1,26 @@
 { nixpkgs-mongodb-pin, ... }: {
 
   nixpkgs.overlays = [
-    (self: super: {
+    (final: prev: {
       mongodb-6_0 = (import nixpkgs-mongodb-pin {
         system = "x86_64-linux";
         config.allowUnfree = true;
       }).mongodb-6_0;
     })
+    # (final: prev: {
+    #   bloop = prev.bloop.overrideAttrs (previousAttrs: rec {
+    #     version = "1.5.17";
+    #     bloop-zsh = prev.fetchurl {
+    #       url =
+    #         "https://github.com/scalacenter/bloop/releases/download/v1.6.0/zsh-completions";
+    #       sha256 = "sha256-WNMsPwBfd5EjeRbRtc06lCEVI2FVoLfrqL82OR0G7/c=";
+    #     };
+    #     bloop-binary = prev.fetchurl {
+    #       url =
+    #         "https://github.com/scalacenter/bloop/releases/download/v1.5.17/bloop-x86_64-pc-linux";
+    #       sha256 = "sha256-f7ZmjDO8L3hcYkaxiwf5BBD44hBiBhNovylM4B308bI=";
+    #     };
+    #   });
+    # })
   ];
 }

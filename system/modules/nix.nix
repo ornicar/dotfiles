b0019065f02a2871a6ca95124e9ad4de8643383f
Thibault Duplessis <t@lichess.org>
2024-07-29 10:59:52 +0200

    nix config to allow nix-build

--- system/modules/nix.nix
+++ system/modules/nix.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, inputs, ... }: {
+{ pkgs, lib, inputs, config, ... }: {
 
   environment.systemPackages = with pkgs; [
     nh
@@ -15,14 +15,14 @@
       # Opinionated: disable global registry
       flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
-      # nix-path = config.nix.nixPath;
+      nix-path = config.nix.nixPath;
     };
     # Opinionated: disable channels
     channel.enable = false;
 
     # Opinionated: make flake registry and nix path match flake inputs
-    # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   home-manager = {

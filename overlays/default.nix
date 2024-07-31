f69a090dd8cbed627665bc75e639e8dfe252ec7b
Thibault Duplessis <t@lichess.org>
2024-07-31 10:43:29 +0200

    nix stockfish overlay disable

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,43 +13,43 @@
       config.allowUnfree = true;
     }).mongodb-6_0;
 
-    stockfish = let
-      arch = "x86-64-bmi2";
-      # see src/evaluate.h for nnue names
-      nnueBigFile = "nn-e8bac1c07a5a.nnue";
-      nnueSmallFile = "nn-37f18f62d772.nnue";
-      nnueBig = prev.fetchurl {
-        name = nnueBigFile;
-        url = "https://tests.stockfishchess.org/api/nn/${nnueBigFile}";
-        sha256 = "sha256-6LrBwHpa6YDPu39gqsi62fGp32QUwakr+y5E6Apk3eg=";
-      };
-      nnueSmall = prev.fetchurl {
-        name = nnueSmallFile;
-        url = "https://tests.stockfishchess.org/api/nn/${nnueSmallFile}";
-        sha256 = "sha256-N/GPYtdy8xB+HWqso4mMEww8hvKrY+ZVX7vKIGNaiZ0=";
-      };
-    in prev.stockfish.overrideAttrs (previousAttrs: {
-      version = "16.1-thib";
-      src = prev.fetchFromGitHub {
-        owner = "official-stockfish";
-        repo = "Stockfish";
-        rev = "stockfish-dev-20240715-e443b245";
-        sha256 = "sha256-DeltJ2a0HmCn5zQL5HAJvQDNhELtNhH8xrOOzlMMt9M=";
-      };
-      postUnpack = ''
-        sourceRoot+=/src
-        echo "Using NNUE big: ${nnueBig}"
-        cp "${nnueBig}" "$sourceRoot/${nnueBigFile}"
-        echo "Using NNUE small: ${nnueSmall}"
-        cp "${nnueSmall}" "$sourceRoot/${nnueSmallFile}"
-      '';
-      makeFlags = [
-        "PREFIX=$(out)"
-        "ARCH=${arch}"
-        "CXX=${prev.stdenv.cc.targetPrefix}c++"
-      ];
-      buildFlags = [ "profile-build" ];
-    });
+    #   stockfish = let
+    #     arch = "x86-64-bmi2";
+    #     # see src/evaluate.h for nnue names
+    #     nnueBigFile = "nn-e8bac1c07a5a.nnue";
+    #     nnueSmallFile = "nn-37f18f62d772.nnue";
+    #     nnueBig = prev.fetchurl {
+    #       name = nnueBigFile;
+    #       url = "https://tests.stockfishchess.org/api/nn/${nnueBigFile}";
+    #       sha256 = "sha256-6LrBwHpa6YDPu39gqsi62fGp32QUwakr+y5E6Apk3eg=";
+    #     };
+    #     nnueSmall = prev.fetchurl {
+    #       name = nnueSmallFile;
+    #       url = "https://tests.stockfishchess.org/api/nn/${nnueSmallFile}";
+    #       sha256 = "sha256-N/GPYtdy8xB+HWqso4mMEww8hvKrY+ZVX7vKIGNaiZ0=";
+    #     };
+    #   in prev.stockfish.overrideAttrs (previousAttrs: {
+    #     version = "16.1-thib";
+    #     src = prev.fetchFromGitHub {
+    #       owner = "official-stockfish";
+    #       repo = "Stockfish";
+    #       rev = "stockfish-dev-20240715-e443b245";
+    #       sha256 = "sha256-DeltJ2a0HmCn5zQL5HAJvQDNhELtNhH8xrOOzlMMt9M=";
+    #     };
+    #     postUnpack = ''
+    #       sourceRoot+=/src
+    #       echo "Using NNUE big: ${nnueBig}"
+    #       cp "${nnueBig}" "$sourceRoot/${nnueBigFile}"
+    #       echo "Using NNUE small: ${nnueSmall}"
+    #       cp "${nnueSmall}" "$sourceRoot/${nnueSmallFile}"
+    #     '';
+    #     makeFlags = [
+    #       "PREFIX=$(out)"
+    #       "ARCH=${arch}"
+    #       "CXX=${prev.stdenv.cc.targetPrefix}c++"
+    #     ];
+    #     buildFlags = [ "profile-build" ];
+    #   });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will

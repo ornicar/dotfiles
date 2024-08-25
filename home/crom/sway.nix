19d779b5d1e6deb70d1f27dce650f63b316c0274
Thibault Duplessis <t@lichess.org>
2024-08-25 13:06:27 +0200

    nix crom sway config

--- home/crom/sway.nix
+++ home/crom/sway.nix
@@ -1,19 +1,25 @@
 { config, lib, pkgs, ... }: {
 
-  wayland.windowManager.sway.config.keybindings =
-    let inherit (config.wayland.windowManager.sway.config) modifier menu;
-    in lib.mkAfter {
-      "${modifier}+space" = "exec ${menu}";
-      "F8" = "exec '${pkgs.playerctl}/bin/playerctl -p spotify play-pause'";
-      "F9" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0'";
-      "F10" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
-      "${modifier}+F10" = "exec 'screenshot.sh clipboard'";
-      "ctrl+f9" = ''exec "sleep 1; pkill -SIGUSR1 swayidle"'';
+  wayland.windowManager.sway = {
+    config = {
+      input = lib.mkAfter {
+        # the glove is a bit too repeaty
+        "type:keyboard".repeat_delay = lib.mkForce "230";
+        # flat profile, no acceleration
+        "type:mouse" = { accel_profile = "flat"; };
+      };
+      keybindings =
+        let inherit (config.wayland.windowManager.sway.config) modifier menu;
+        in lib.mkAfter {
+          "${modifier}+space" = "exec ${menu}";
+          "F8" = "exec '${pkgs.playerctl}/bin/playerctl -p spotify play-pause'";
+          "F9" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0'";
+          "F10" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
+          "${modifier}+F10" = "exec 'screenshot.sh clipboard'";
+          "ctrl+f9" = ''exec "sleep 1; pkill -SIGUSR1 swayidle"'';
+        };
     };
-
-  # the glove is a bit too repeaty
-  wayland.windowManager.sway.config.input."type:keyboard".repeat_delay =
-    lib.mkForce "230";
+  };
 
   services.swayidle = {
     enable = true;

b758c5e539352a36cb9f6b6aaa8d42517a75f6c8
Thibault Duplessis <t@lichess.org>
2024-08-26 20:39:23 +0200

    fw sway brightness control

--- home/fw/sway.nix
+++ home/fw/sway.nix
@@ -17,8 +17,8 @@
         spotify = "${pkgs.playerctl}/bin/playerctl -p spotify";
       in lib.mkAfter {
         "${modifier}+equal" = "exec ${menu}";
-        "XF86MonBrightnessDown" = "exec 'light -U 10'";
-        "XF86MonBrightnessUp" = "exec 'light -A 10'";
+        "XF86MonBrightnessDown" = "exec 'light -U 15'";
+        "XF86MonBrightnessUp" = "exec 'light -A 15'";
         "XF86AudioRaiseVolume" =
           "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
         "XF86AudioLowerVolume" =

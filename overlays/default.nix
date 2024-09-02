085111d2333dcce2003a89e2307c2ffa0392751d
Thibault Duplessis <t@lichess.org>
2024-09-02 15:34:21 +0200

    nix remove unused unstable-packages overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,12 +2,12 @@
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
-  };
+  # unstable-packages = final: _prev: {
+  #   unstable = import inputs.nixpkgs-unstable {
+  #     system = final.system;
+  #     config.allowUnfree = true;
+  #   };
+  # };
 
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs final.pkgs;
--- system/modules/common.nix
+++ system/modules/common.nix
@@ -7,7 +7,6 @@
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
-      outputs.overlays.unstable-packages
     ];
   };
 

1b80970d346df16900f439265dda78e51b538f9c
Thibault Duplessis <t@lichess.org>
2024-10-12 11:53:12 +0200

    nix starcraft on fw

--- home/fw/default.nix
+++ home/fw/default.nix
@@ -1,6 +1,6 @@
 # frame.work 16 home config
 { ... }: {
-  imports = [ ../common.nix ./sway.nix ./waybar.nix ];
+  imports = [ ../common.nix ./sway.nix ./waybar.nix ../modules/wine.nix ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.05";

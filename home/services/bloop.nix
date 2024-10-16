1b337ecd1534212814f04fdbc4baf9850df3103d
Thibault Duplessis <t@lichess.org>
2024-10-16 09:25:52 +0200

    nix bloop service, still not working

--- home/services/bloop.nix
+++ home/services/bloop.nix
@@ -1,14 +1,19 @@
 { pkgs, lib, config, ... }: {
+
   systemd.user.services.bloop = {
     Unit = { Description = "Bloop Scala build server"; };
     Service = {
       Environment =
         [ "PATH=${lib.makeBinPath [ config.programs.java.package ]}" ];
-      ExecStart = "${pkgs.bloop}/bin/bloop";
+      ExecStart = "${pkgs.bloop}/bin/bloop start";
+      StandardOutput = "journal";
+      StandardError = "journal";
+      SyslogIdentifier = "bloop";
       ExecStop = "${pkgs.psmisc}/bin/fuser -k 8212/tcp -TERM";
     };
     Install = { WantedBy = [ "default.target" ]; };
   };
+
   # https://scalacenter.github.io/bloop/docs/server-reference#custom-java-options
   home.file.".bloop/bloop.json".text = ''
     {

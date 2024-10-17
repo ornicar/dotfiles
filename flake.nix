e063b493313e1e1849b6f24333b36c11e2da8600
Thibault Duplessis <t@lichess.org>
2024-10-17 08:34:41 +0200

    nix stable source is not needed

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     nixpkgs-mongodb-pin.url =

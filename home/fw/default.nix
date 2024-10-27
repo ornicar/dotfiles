f4dba076208d3e059cce334e4f237e474f968729
Thibault Duplessis <t@lichess.org>
2024-10-27 21:51:37 +0100

    nix fw easyeffects

--- home/fw/default.nix
+++ home/fw/default.nix
@@ -1,6 +1,7 @@
 # frame.work 16 home config
 { ... }: {
-  imports = [ ../common.nix ../easyeffects.nix ./sway.nix ./waybar.nix ];
+  imports =
+    [ ../common.nix ../modules/easyeffects.nix ./sway.nix ./waybar.nix ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.05";

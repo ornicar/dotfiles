4fa53935be6724b16da5da23bd1f915220529790
Thibault Duplessis <t@lichess.org>
2024-11-04 21:08:31 +0100

    nix zed module and config

---
+++ home/modules/zed.nix
@@ -0,0 +1,8 @@
+{ pkgs, config, ... }: {
+  home.packages = with pkgs; [ zed-editor ];
+
+  # Use the external dotfiles zed config for quicker hacking
+  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/dotfiles/zed";
+}
+

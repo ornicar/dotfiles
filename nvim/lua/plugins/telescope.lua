1401774583bf72ddd16e1f983713ed842e169981
Thibault Duplessis <t@lichess.org>
2024-11-12 21:10:58 +0100

    nvim telescope-undo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -104,4 +104,31 @@ return {
     end,
   },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "debugloop/telescope-undo.nvim",
+    keys = {
+      { -- lazy style key map
+        "<leader>U",
+        "<cmd>Telescope undo<cr>",
+        desc = "undo history",
+      },
+    },
+    opts = {
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.8,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+      -- defaults, as well as each extension).
+      require("telescope").load_extension("undo")
+    end,
+  },
 }

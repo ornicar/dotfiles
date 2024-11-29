357fffba986e64c78bcedf7e80e45104e86c7355
Thibault Duplessis <t@lichess.org>
2024-11-29 15:39:20 +0100

    Revert "nvim line-number-change-mode.nvim"
    This reverts commit 6ea8ae93f57cac7dc6c0fe3f1be5f3e22416e834.

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -97,49 +97,6 @@ return {
       return {}
     end,
   },
-  {
-    "sethen/line-number-change-mode.nvim",
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-      })
-      local palette = require("catppuccin.palettes").get_palette("mocha")
-
-      if palette == nil then
-        return nil
-      end
-
-      require("line-number-change-mode").setup({
-        mode = {
-          i = {
-            bg = palette.green,
-            fg = palette.mantle,
-            bold = true,
-          },
-          n = {
-            bg = palette.blue,
-            fg = palette.mantle,
-            bold = true,
-          },
-          R = {
-            bg = palette.maroon,
-            fg = palette.mantle,
-            bold = true,
-          },
-          v = {
-            bg = palette.mauve,
-            fg = palette.mantle,
-            bold = true,
-          },
-          V = {
-            bg = palette.mauve,
-            fg = palette.mantle,
-            bold = true,
-          },
-        },
-      })
-    end,
-  },
 
   -- dashboard
   {

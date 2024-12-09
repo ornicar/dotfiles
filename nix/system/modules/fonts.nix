a50ec58db323c2884edf6dc801f3bb2e8fccd2ed
Thibault Duplessis <t@lichess.org>
2024-12-09 15:19:12 +0100

    nix font-awesome

--- nix/system/modules/fonts.nix
+++ nix/system/modules/fonts.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   # more in stylix.nix
   fonts.packages = with pkgs; [
-    # font-awesome # installed for waybar icons
+    font-awesome # installed for waybar icons
     jetbrains-mono # terminal
     # iosevka-bin
     nerd-fonts.jetbrains-mono

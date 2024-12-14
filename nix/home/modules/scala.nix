19d519f2764c54b27a396f816a4c83dcbe911af5
Thibault Duplessis <t@lichess.org>
2024-12-14 08:45:56 +0100

    nix bloop config

--- nix/home/modules/scala.nix
+++ nix/home/modules/scala.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }: {
 
   # https://scalacenter.github.io/bloop/docs/server-reference#custom-java-options
-  home.file.".bloop/bloop.json".text = ''
-    {
-      "javaOptions": ["-Xms4g", "-Xmx14G", "-XX:+UseG1GC", "-Xss2m", "-XX:InitialCodeCacheSize=512m", "-XX:ReservedCodeCacheSize=512m"]
-    }
-  '';
 
   home.file = {
     "./.sbtrc".text = ''
--- nix/system/modules/shell.nix
+++ nix/system/modules/shell.nix
@@ -15,6 +15,7 @@
     XDG_STATE_HOME = "$HOME/.local/state";
     XDG_BIN_HOME = "$HOME/.local/bin";
     PATH = [ "${XDG_BIN_HOME}" ];
+    BLOOP_JAVA_OPTS = "-Xmx16G";
   };
   environment.systemPackages = with pkgs; [
     kitty

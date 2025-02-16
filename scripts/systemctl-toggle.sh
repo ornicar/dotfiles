3885ba6c1641e5df0c5600471af18111b86a662f
Thibault Duplessis <t@lichess.org>
2025-02-16 14:20:29 +0100

    remove comment

--- scripts/systemctl-toggle.sh
+++ scripts/systemctl-toggle.sh
@@ -3,9 +3,6 @@
 service=$1
 systemctl="systemctl --user"
 
-# start service if it's not running
-# or stop service if it's running
-
 if $systemctl is-active --quiet $service; then
   $systemctl stop $service
 else

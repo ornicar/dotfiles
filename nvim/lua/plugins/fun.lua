1b4bfeaa65fa68b346f1e6207abdee99f4b8c7a2
Thibault Duplessis <t@lichess.org>
2025-02-20 16:20:08 +0100

    nvim fun

--- nvim/lua/plugins/fun.lua
+++ nvim/lua/plugins/fun.lua
@@ -35,5 +35,14 @@ return {
       -- more opts
     },
   },
-  { "nvzone/timerly", cmd = "TimerlyToggle", dependencies = { "nvzone/volt" } },
+  -- { "nvzone/timerly", cmd = "TimerlyToggle", dependencies = { "nvzone/volt" } },
+  -- {
+  --   "jackplus-xyz/player-one.nvim",
+  --   ---@type PlayerOne.Config
+  --   opts = {
+  --     -- your configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --   },
+  -- },
 }

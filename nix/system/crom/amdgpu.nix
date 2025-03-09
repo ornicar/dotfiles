cf379926ad65f15053c9e119c244ea57412685b3
Thibault Duplessis <t@lichess.org>
2025-03-09 12:56:45 +0100

    nix no longer set gpu power level manually

--- nix/system/crom/amdgpu.nix
+++ nix/system/crom/amdgpu.nix
@@ -1,22 +1,8 @@
 { pkgs, ... }: {
-  # https://wiki.archlinux.org/title/AMDGPU#Boot_parameter
-  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xfffd7fff" ];
-
-  systemd.services.thib-amdgpu-set-power = {
-    script = let drm-dir = "/sys/class/drm/card1/device";
-    in ''
-      ${pkgs.coreutils}/bin/echo "manual" > ${drm-dir}/power_dpm_force_performance_level
-      ${pkgs.coreutils}/bin/echo "2" > ${drm-dir}/pp_power_profile_mode
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-  };
 
   systemd.services.thib-amdgpu-monitor = {
     script = let
-      device = "amdgpu-pci-0800";
+      device = "amdgpu-pci-0300";
       sensors = "${pkgs.lm_sensors}/bin/sensors";
       sed = "${pkgs.gnused}/bin/sed";
     in ''
@@ -31,11 +17,26 @@
     '';
   };
 
-  systemd.timers.thib-amdgpu-set-power = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "*:0/5"; # every 5 minutes
-      Unit = "thib-amdgpu-set-power.service";
-    };
-  };
+  # https://wiki.archlinux.org/title/AMDGPU#Boot_parameter
+  # boot.kernelParams = [ "amdgpu.ppfeaturemask=0xfffd7fff" ];
+  #
+  # systemd.services.thib-amdgpu-set-power = {
+  #   script = let drm-dir = "/sys/class/drm/card1/device";
+  #   in ''
+  #     ${pkgs.coreutils}/bin/echo "manual" > ${drm-dir}/power_dpm_force_performance_level
+  #     ${pkgs.coreutils}/bin/echo "2" > ${drm-dir}/pp_power_profile_mode
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     User = "root";
+  #   };
+  # };
+
+  # systemd.timers.thib-amdgpu-set-power = {
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig = {
+  #     OnCalendar = "*:0/5"; # every 5 minutes
+  #     Unit = "thib-amdgpu-set-power.service";
+  #   };
+  # };
 }

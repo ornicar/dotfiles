e52025c813113ee3943ec0e1507d712166bd436e
Thibault Duplessis <t@lichess.org>
2025-03-28 08:11:30 +0100

    neovim 0.11 with nixpkgs#master

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     nixpkgs-mongodb-pin.url =
       "github:NixOS/nixpkgs/fd4f8ce9fc5a4a1140b6648d576c42ce41caf8fe";

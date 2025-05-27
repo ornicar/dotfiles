bfe57f5f04bff6509ab7b7ceae383973d6dcc055
Thibault Duplessis <t@lichess.org>
2025-05-27 10:08:20 +0200

    Revert "nix remove stockfish overlay"
    This reverts commit 4abab4cc784f62da1822b6dd22cb233bb3b0a9a9.

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -40,5 +40,42 @@
           --replace /usr/bin/env "${prev.coreutils}/bin/env"
       '';
     });
+
+    stockfish = let
+      version = "17";
+      arch = "x86-64-avx512";
+      # see src/evaluate.h for nnue names
+      nnueBigFile = "nn-1111cefa1111.nnue";
+      nnueSmallFile = "nn-37f18f62d772.nnue";
+      nnueBig = prev.fetchurl {
+        name = nnueBigFile;
+        url = "https://tests.stockfishchess.org/api/nn/${nnueBigFile}";
+        sha256 = "sha256-ERHO+hERa3cWG9SxTatMUPJuWSDHVvSGFZK+Pc1t4XQ=";
+      };
+      nnueSmall = prev.fetchurl {
+        name = nnueSmallFile;
+        url = "https://tests.stockfishchess.org/api/nn/${nnueSmallFile}";
+        sha256 = "sha256-N/GPYtdy8xB+HWqso4mMEww8hvKrY+ZVX7vKIGNaiZ0=";
+      };
+    in prev.stockfish.overrideAttrs (previousAttrs: {
+      version = "${version}-thib";
+      src = prev.fetchFromGitHub {
+        owner = "official-stockfish";
+        repo = "Stockfish";
+        rev = "sf_${version}";
+        sha256 = "sha256-oXvLaC5TEUPlHjhm7tOxpNPY88QxYHFw+Cev3Q8NEeQ=";
+      };
+      postUnpack = ''
+        sourceRoot+=/src
+        cp "${nnueBig}" "$sourceRoot/${nnueBigFile}"
+        cp "${nnueSmall}" "$sourceRoot/${nnueSmallFile}"
+      '';
+      makeFlags = [
+        "PREFIX=$(out)"
+        "ARCH=${arch}"
+        "CXX=${prev.stdenv.cc.targetPrefix}c++"
+      ];
+      buildFlags = [ "build" ];
+    });
   };
 }

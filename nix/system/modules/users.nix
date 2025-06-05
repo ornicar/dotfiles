6999b958e2318ca769e7d3f16b9ab38210dd55fd
Thibault Duplessis <t@lichess.org>
2025-06-05 12:33:59 +0200

    nix user dialout group
    for bafang

--- nix/system/modules/users.nix
+++ nix/system/modules/users.nix
@@ -2,7 +2,7 @@
   users.users = {
     thib = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "video" "nginx" ];
+      extraGroups = [ "wheel" "video" "nginx" "dialout" ];
       homeMode = "750"; # for nginx to read assets
     };
   };

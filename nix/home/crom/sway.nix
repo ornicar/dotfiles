b19c4bc3f9a9d21db3634ff3401cb60425f27d94
Thibault Duplessis <t@lichess.org>
2025-08-14 09:39:08 +0200

    sway volume

--- nix/home/crom/sway.nix
+++ nix/home/crom/sway.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, ... }: {
 
   wayland.windowManager.sway = {
-    config = {
+    config = let
+      volumeDec = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0'";
+      volumeInc = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
+    in {
       input = lib.mkAfter {
         # the glove is a bit too repeaty
         "type:keyboard".repeat_delay = lib.mkForce "250";
@@ -13,11 +16,17 @@
         in lib.mkAfter {
           "${modifier}+space" = "exec ${menu}";
           "F8" = "exec '${pkgs.playerctl}/bin/playerctl -p spotify play-pause'";
-          "F9" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 1.0'";
-          "F10" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0'";
+          "F9" = volumeDec;
+          "F10" = volumeInc;
           "${modifier}+F10" = "exec 'screenshot.sh clipboard'";
           "ctrl+f9" = ''exec "sleep 1; pkill -SIGUSR1 swayidle"'';
         };
+      modes = {
+        gaming = {
+          "F9" = volumeDec;
+          "F10" = volumeInc;
+        };
+      };
     };
   };
 

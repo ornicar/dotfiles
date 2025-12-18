551e76534a9a61f8f5cd350f14111271c9850280
Thibault Duplessis <t@lichess.org>
2025-12-18 17:01:39 +0100

    nix comment stockfish overlay

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -69,41 +69,41 @@
     #   };
     # });
 
-    stockfish = let
-      version = "17.1";
-      arch = "x86-64-avx512";
-      # see src/evaluate.h for nnue names
-      nnueBigFile = "nn-1c0000000000.nnue";
-      nnueSmallFile = "nn-37f18f62d772.nnue";
-      nnueBig = prev.fetchurl {
-        name = nnueBigFile;
-        url = "https://tests.stockfishchess.org/api/nn/${nnueBigFile}";
-        sha256 = "sha256-HAAAAAAApn1imZnZMtDDc/dFDOQ80S0FYoaPTq+a4q0=";
-      };
-      nnueSmall = prev.fetchurl {
-        name = nnueSmallFile;
-        url = "https://tests.stockfishchess.org/api/nn/${nnueSmallFile}";
-        sha256 = "sha256-N/GPYtdy8xB+HWqso4mMEww8hvKrY+ZVX7vKIGNaiZ0=";
-      };
-    in prev.stockfish.overrideAttrs (previousAttrs: {
-      version = "${version}";
-      src = prev.fetchFromGitHub {
-        owner = "official-stockfish";
-        repo = "Stockfish";
-        rev = "sf_${version}";
-        sha256 = "sha256-c8o1d7/yPnF3Eo7M/MSzYuYQr2qt2tIwyu7WfuKMAzg=";
-      };
-      postUnpack = ''
-        sourceRoot+=/src
-        cp "${nnueBig}" "$sourceRoot/${nnueBigFile}"
-        cp "${nnueSmall}" "$sourceRoot/${nnueSmallFile}"
-      '';
-      makeFlags = [
-        "PREFIX=$(out)"
-        "ARCH=${arch}"
-        "CXX=${prev.stdenv.cc.targetPrefix}c++"
-      ];
-      buildFlags = [ "build" ];
-    });
+    # stockfish = let
+    #   version = "17.1";
+    #   arch = "x86-64-avx512";
+    #   # see src/evaluate.h for nnue names
+    #   nnueBigFile = "nn-1c0000000000.nnue";
+    #   nnueSmallFile = "nn-37f18f62d772.nnue";
+    #   nnueBig = prev.fetchurl {
+    #     name = nnueBigFile;
+    #     url = "https://tests.stockfishchess.org/api/nn/${nnueBigFile}";
+    #     sha256 = "sha256-HAAAAAAApn1imZnZMtDDc/dFDOQ80S0FYoaPTq+a4q0=";
+    #   };
+    #   nnueSmall = prev.fetchurl {
+    #     name = nnueSmallFile;
+    #     url = "https://tests.stockfishchess.org/api/nn/${nnueSmallFile}";
+    #     sha256 = "sha256-N/GPYtdy8xB+HWqso4mMEww8hvKrY+ZVX7vKIGNaiZ0=";
+    #   };
+    # in prev.stockfish.overrideAttrs (previousAttrs: {
+    #   version = "${version}";
+    #   src = prev.fetchFromGitHub {
+    #     owner = "official-stockfish";
+    #     repo = "Stockfish";
+    #     rev = "sf_${version}";
+    #     sha256 = "sha256-c8o1d7/yPnF3Eo7M/MSzYuYQr2qt2tIwyu7WfuKMAzg=";
+    #   };
+    #   postUnpack = ''
+    #     sourceRoot+=/src
+    #     cp "${nnueBig}" "$sourceRoot/${nnueBigFile}"
+    #     cp "${nnueSmall}" "$sourceRoot/${nnueSmallFile}"
+    #   '';
+    #   makeFlags = [
+    #     "PREFIX=$(out)"
+    #     "ARCH=${arch}"
+    #     "CXX=${prev.stdenv.cc.targetPrefix}c++"
+    #   ];
+    #   buildFlags = [ "build" ];
+    # });
   };
 }

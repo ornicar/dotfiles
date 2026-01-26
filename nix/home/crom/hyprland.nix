e40ec8e98950da30c99fe6e2b5593a97c7a99fe6
Thibault Duplessis <t@lichess.org>
2026-01-26 14:21:24 +0100

    crom idle

--- nix/home/crom/hyprland.nix
+++ nix/home/crom/hyprland.nix
@@ -32,4 +32,21 @@
         };
       };
   };
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        lock_cmd = "hyprlock";
+      };
+      listener = [
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
 }

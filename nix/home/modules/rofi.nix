f3e1222a0c3921ef46a4d2e87091fe2ace025830
Thibault Duplessis <t@lichess.org>
2026-02-02 20:00:41 +0100

    rofi modes

--- nix/home/modules/rofi.nix
+++ nix/home/modules/rofi.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
   programs.rofi = {
     enable = true;
     theme = "material";
+    plugins = [
+      pkgs.rofi-emoji
+      pkgs.rofi-calc
+    ];
+    modes = [
+      "window"
+      "run"
+      "emoji"
+      "calc"
+    ];
   };
   stylix.targets.rofi.enable = false;
 }

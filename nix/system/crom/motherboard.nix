6ac9cdcf151a07e82f6ba965fb67f42060eb22ad
Thibault Duplessis <t@lichess.org>
2026-02-19 13:47:04 +0100

    crom disable wifi module
    that randomly crashes

--- nix/system/crom/motherboard.nix
+++ nix/system/crom/motherboard.nix
@@ -5,6 +5,8 @@
   boot.kernelModules = [ "nct6687" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ nct6687d ];
 
+  boot.blacklistedKernelModules = [ "mt7921e" ];
+
   services.hardware.openrgb = {
     enable = true;
     motherboard = "amd";

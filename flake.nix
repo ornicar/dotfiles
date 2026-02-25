0cb96db095372f6712a0761df6778cad1ee6b0a0
Thibault Duplessis <t@lichess.org>
2026-02-25 20:06:30 +0100

    nixpkgs master to fix pico failed tests

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Thib's NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 

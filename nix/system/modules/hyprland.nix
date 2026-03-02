9c7ec02fc177c3f7f7882c5b527bd6b47ec9d4ba
Thibault Duplessis <t@lichess.org>
2026-03-02 08:53:29 +0100

    nixos hyprland - compiled - obs works

--- nix/system/modules/hyprland.nix
+++ nix/system/modules/hyprland.nix
@@ -9,10 +9,10 @@
 
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    # portalPackage =
+    # inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     withUWSM = true; # recommended for most users
     xwayland.enable = false;
   };

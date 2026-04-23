abeb5509ee0d9194f6bc6ca2bb5a2aa3dcceda28
Thibault Duplessis <t@lichess.org>
2026-04-23 08:31:25 +0200

    nvim remove firenvim

--- nvim/lua/plugins/integration.lua
+++ nvim/lua/plugins/integration.lua
@@ -10,29 +10,4 @@ return {
       require("kitty-scrollback").setup()
     end,
   },
-  {
-    "glacambre/firenvim",
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    lazy = not vim.g.started_by_firenvim,
-    build = function()
-      vim.fn["firenvim#install"](0)
-    end,
-    config = function()
-      vim.g.firenvim_config = {
-        -- config values, like in my case:
-        localSettings = {
-          [".*"] = {
-            takeover = "never",
-            -- selector = 'textarea:not([readonly], [aria-readonly]), div[role="textbox"]'
-            selector = "textarea:not([readonly], [aria-readonly])",
-            cmdline = "firenvim",
-          },
-          ["https://google.com/"] = { takeover = "never", priority = 1 },
-        },
-      }
-    end,
-  },
-  { "noice.nvim", cond = not vim.g.started_by_firenvim },
-  { "lualine.nvim", cond = not vim.g.started_by_firenvim },
 }

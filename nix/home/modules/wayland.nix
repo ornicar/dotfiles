0c9e7c44559234c692427a27966a12a4d9944898
Thibault Duplessis <t@lichess.org>
2026-08-07 08:53:48 +0200

    nix remove mako

--- nix/home/modules/wayland.nix
+++ nix/home/modules/wayland.nix
@@ -6,17 +6,4 @@
   services.cliphist = {
     enable = true;
   };
-
-  services.mako = {
-    enable = true;
-    settings = {
-      default-timeout = 5000;
-      ignore-timeout = false;
-      outer-margin = 30;
-      padding = "15 20 15 20";
-      border-size = 2;
-      border-radius = 10;
-    };
-  };
-  stylix.targets.mako.enable = true;
 }

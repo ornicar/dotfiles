7cd85e294a5c806f4ce3256cd3c79101a1935403
Thibault Duplessis <t@lichess.org>
2026-09-05 14:37:38 +0200

    nix experimental-features new syntax

--- nix/system/modules/nix.nix
+++ nix/system/modules/nix.nix
@@ -20,7 +20,10 @@
     {
       settings = {
         # Enable flakes and new 'nix' command
-        experimental-features = "nix-command flakes";
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
         # Opinionated: disable global registry
         flake-registry = "";
         # Workaround for https://github.com/NixOS/nix/issues/9574
